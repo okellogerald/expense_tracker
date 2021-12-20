d1f4869a0c1b97be710efe5245eead0568acec20
okellogeralddev@gmail.com <okellogeralddev@gmail.com>
2021-12-20 20:23:18 +0300

    added a new font

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,48 +7,49 @@ const kRecords = 'records';
 const kTotalRecords = 'total_records';
 const kExpense = 'expense';
 const kIncome = 'income';
-const kFontFam2 = 'Louis';
+const kFontFam4 = 'Louis';
 const kFontFam3 = 'Outfit';
+const kFontFam2 = 'Glacial';
 
 final kDefaultCategoriesList = <Category>[
   Category(
       codePoint: AppIcons.home.codePoint,
       title: 'Rent',
-      id: '0',
+      id: 'WOfZu0q6un',
       type: kIncome),
   Category(
       codePoint: AppIcons.award_2.codePoint,
       title: 'Award',
-      id: '1',
+      id: '8QzyrePahi',
       type: kIncome),
   Category(
       codePoint: AppIcons.coupon.codePoint,
       title: 'Coupon',
-      id: '2',
+      id: 'iNgh6rV7Ht',
       type: kIncome),
   Category(
       codePoint: AppIcons.education.codePoint,
       title: 'Education',
-      id: '3',
+      id: 'nYh5nNjLEf',
       type: kExpense),
   Category(
       codePoint: AppIcons.tv.codePoint,
       title: 'Electronics',
-      id: '4',
+      id: 'tEJr6yWkYr',
       type: kExpense),
   Category(
       codePoint: AppIcons.airplane.codePoint,
       title: 'Flight',
-      id: '5',
+      id: 'DkKTijVDJv',
       type: kExpense),
   Category(
       codePoint: AppIcons.printer.codePoint,
       title: 'Stationaries',
-      id: '6',
+      id: 'hhCHRYeNI4',
       type: kExpense),
   Category(
       codePoint: AppIcons.groceries.codePoint,
       title: 'Groceries',
-      id: '7',
+      id: 'Lgby3i7P8F',
       type: kExpense),
 ];
